02c1bf2cd4be2fcebc6b4956019bd30b3671ec37
Md Hasanul Karim <hasanlink71@yahoo.com>
2022-10-27 21:01:59 +0600

    environment added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC0cisHlLQlcDOC_AwxmOY05enP2qjWw6U",
-  authDomain: "dragon-news-d8f1c.firebaseapp.com",
-  projectId: "dragon-news-d8f1c",
-  storageBucket: "dragon-news-d8f1c.appspot.com",
-  messagingSenderId: "575035330408",
-  appId: "1:575035330408:web:9f847ac1e0fe175fc810dd"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
