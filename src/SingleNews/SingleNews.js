adc8d41e6dd3649cdd8d0b9367cd5f1aea045b45
Md Hasanul Karim <hasanlink71@yahoo.com>
2022-10-30 00:14:34 +0600

    news details edited

--- src/SingleNews/SingleNews.js
+++ src/SingleNews/SingleNews.js
@@ -3,21 +3,30 @@ import useTitle from '../hooks/useTitle';
 
 const SingleNews = ({ n }) => {
   console.log(n);
-  const { title, details, author, total_view } = n;
+  const { title, details, author, total_view, thumbnail_url } = n;
   useTitle('internal news')
   return (
     <div className='py-1'>
-      <div class="card">
-        <h5 class="card-header">Published: {author.published_date}</h5>
-        <div class="card-body">
-          <h5 class="card-title">{title}</h5>
-          <p class="card-text">{details}</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+      <div className="card">
+        <h5 className="card-header">Published: {author.published_date}</h5>
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <div className='d-flex'>
+            <img className="img-thumbnail rounded float-left me-2" src={thumbnail_url} alt="Card image cap" />
+            <p className="card-text">{details}</p>
+            {/* <div className="card" >
+              <img className="card-img-top" src={thumbnail_url} alt="Card image cap" />
+              <div className="card-body">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div> */}
+          </div>
+          <a href="#" className="btn btn-primary">Go somewhere</a>
         </div>
-        <h5 class="card-header">Total view: {total_view
+        <h5 className="card-header">Total view: {total_view
         }</h5>
       </div>
-    </div>
+    </div >
   );
 };
 
